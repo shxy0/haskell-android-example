8ad6b4053498e239a846089d58818287805c2994
shxy0 <anonymous>
2019-03-30 08:08:53 +0000

    haskell dyn lib builded

---
+++ hs/dist/build/libhaskell.so/libhaskell.so-tmp/HaskellActivity_stub.h
@@ -0,0 +1,10 @@
+#include "HsFFI.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern void Java_com_example_hellojni_HelloJni_onCreateHS(HsPtr a1, HsPtr a2, HsPtr a3);
+extern void HaskellActivity_d6zf(void *cif STG_UNUSED, void* resp, void** args, void* the_stableptr);
+#ifdef __cplusplus
+}
+#endif
+
